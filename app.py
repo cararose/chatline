bd15bdb9c36bcee5f67d209c2b5c922420c84f33
Andrew Hyder <hyd415@gmail.com>
2014-12-04 16:21:55 -0800

    Set as xml

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, Response
 app = Flask(__name__)
 
 @app.route("/incoming_call")
 def incoming_call():
     # This handles the incoming calls
-    twiml = '''
-            <?xml version="1.0" encoding="UTF-8"?>
-            <Response>
-                <Gather timeout="10" finishOnKey="*">
-                    <Say>Please enter your pin number and then press star.</Say>
-                </Gather>
-            </Response>
+    twiml = '''<?xml version="1.0" encoding="UTF-8"?>
+                <Response>
+                    <Gather timeout="10" finishOnKey="*">
+                        <Say>Please enter your pin number and then press star.</Say>
+                    </Gather>
+                </Response>
             '''
 
     return Response(twiml, mimetype='text/xml')
+
+if __name__ == "__main__":
+    app.run(debug=True)
